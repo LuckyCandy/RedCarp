91c900e079cf63b9936c248421d985d07e524ab3
Damon <1157581951@qq.com>
2019-02-24 23:18:42 +0800

    着陆页设计

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damon
+ * Date: 2019-02-24
+ * Time: 22:06
+ */
+
+namespace App\Http\Controllers;
+
+
+class LandingController
+{
+
+}
---
+++ resources/views/landing/layout.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damon
+ * Date: 2019-02-24
+ * Time: 19:40
+ */
